058f2abe7430639e11510b8b2f8c36751d8e0512
Bohdan Benetskyi <benetskyybogdan@gmail.com>
2020-09-07 15:14:49 +0200

    Add IcoMoon Icons Font

---
+++ SeptemberUIChallenge/SeptemberUIChallenge/Resources/Fonts/IconCodes.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace SeptemberUIChallenge.Resources.Fonts
+{
+    public static class IconCodes
+    {
+        public const string Images = "\ue901";
+        public const string BrokenHeart = "\ue902";
+        public const string Heart = "\ue903";
+    }
+}
